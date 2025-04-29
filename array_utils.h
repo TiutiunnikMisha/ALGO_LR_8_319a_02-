0f6ab61b0b82177c99ba4ee5a276099e9dcd8801
TiutiunnikMisha <m.a.tiutiunnyk@student.khai.edu>
2025-04-29 13:31:35 +0300

    array_utils.h

---
+++ array_utils.h
@@ -0,0 +1,11 @@
+#ifndef ARRAY_UTILS_H
+#define ARRAY_UTILS_H
+#include <string>
+using namespace std;
+
+// Прототип функції для реверсу масиву чисел
+void reverseArrayFromFile(const string& inputFilename, const string& outputFilename);
+// Прототип функції для сортування масиву символів
+void sortArrayFromFile(const string& inputFilename, const string& outputFilename);
+
+#endif // ARRAY_UTILS_H
