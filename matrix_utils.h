ca1fca4079afa0f9224b0b9f21f69f2bdb09d2c6
TiutiunnikMisha <m.a.tiutiunnyk@student.khai.edu>
2025-04-29 13:32:00 +0300

    matrix_utils.h

---
+++ matrix_utils.h
@@ -0,0 +1,9 @@
+#ifndef MATRIX_UTILS_H
+#define MATRIX_UTILS_H
+#include <string>
+using namespace std;
+
+// Прототип функції обробки матриці (обчислення сум рядків)
+void processMatrixFromFile(const string& filename);
+
+#endif // MATRIX_UTILS_H
